b2b09fa85c7a7abed5063acbd020d272eb58fe9d
zitzmah <haleyzitzmann@gmail.com>
2023-12-25 16:12:00 -0700

    router added

---
+++ controllers/pokemon.js
@@ -0,0 +1,51 @@
+const express = require("express")
+const router = express.Router()
+const Pokemon = require("../models/pokemon.js")
+
+//INDEX
+router.get("/", (req, res) => {
+    res.render('index.ejs', { data: Pokemon })
+})
+
+//NEW
+router.get("/new", (req, res) => {
+    res.render("new.ejs")
+})
+
+//DELETE
+router.delete("/:id", (req, res)=>{
+    const id = req.params.id
+    Pokemon.splice(id, 1)
+    res.redirect("/")
+})
+
+//UPDATE
+router.put("/:id", (req, res)=>{
+    const id = req.params.id
+    const body = req.body
+    Pokemon[id] = body
+    res.redirect("/")
+})
+
+//CREATE
+router.post("/", (req, res)=>{
+    const body =req.body
+    Pokemon.push(body)
+    res.redirect("/")
+})
+
+//EDIT
+router.get("/:id/edit", (req, res)=>{
+    const id = req.params.id
+    const pokemon = Pokemon[id]
+    res.render("edit.ejs")
+})
+
+//SHOW
+router.get("/:id", (req, res) => {
+    res.render("show.ejs", { data: Pokemon[req.params.id] })
+})
+
+//EXPORT THE ROUTER
+module.exports = router
+
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 
+const pokemonRouter = require("./controllers/pokemon.js")
 const Pokemon = require("./models/pokemon.js")
 const app = express()
 
@@ -7,43 +8,7 @@ const app = express()
 //MIDDLEWARE
 app.use(express.static("public"))
 app.use(express.urlencoded({extended: true}))
-
-//INDEX
-app.get("/", (req, res) => {
-    res.render('index.ejs', { data: Pokemon })
-})
-
-//NEW
-app.get("/new", (req, res) => {
-    res.render("new.ejs")
-})
-
-//DELETE
-
-
-//UPDATE
-
-
-//CREATE
-app.post("/", (req, res)=>{
-    const body =req.body
-    Pokemon.push(body)
-    res.redirect("/")
-})
-
-//EDIT
-
-
-//SHOW
-app.get("/:id", (req, res) => {
-    res.render("show.ejs", { data: Pokemon[req.params.id] })
-})
-
-// app.get("/Pokemon/:id", (req, res) => {
-//     const id = req.params.id
-//     const poke=Pokemon[id]
-//     res.render("show.ejs", {pokemon})
-// })
+app.use("/", pokemonRouter)
 
 //*********************** */
 // SERVER LISTENER
